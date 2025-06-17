260bab12cc2d20834aa588dea63848c27b7eef5c
lexgp <lexgp@yandex.ru>
2025-06-17 19:56:02 +0300

    Проблема передачи данных между тасками

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -11,23 +11,38 @@ from etl.train_model import train
 from etl.evaluate import evaluate
 from etl.save_results import save
 
-def step_1_load():
-    global df
+# 1. Загрузка данных
+def step_1_load(**context):
     df = load()
+    context['ti'].xcom_push(key='df', value=df)
 
-def step_2_preprocess():
-    global X, y
+# 2. Предобработка
+def step_2_preprocess(**context):
+    df = context['ti'].xcom_pull(key='df', task_ids='load_data')
     X, y = preprocess(df)
+    context['ti'].xcom_push(key='X', value=X)
+    context['ti'].xcom_push(key='y', value=y)
 
-def step_3_train():
-    global model, X_test, y_test
+# 3. Обучение
+def step_3_train(**context):
+    X = context['ti'].xcom_pull(key='X', task_ids='preprocess')
+    y = context['ti'].xcom_pull(key='y', task_ids='preprocess')
     model, X_test, y_test = train(X, y)
+    context['ti'].xcom_push(key='model', value=model)
+    context['ti'].xcom_push(key='X_test', value=X_test)
+    context['ti'].xcom_push(key='y_test', value=y_test)
 
-def step_4_evaluate():
+# 4. Оценка
+def step_4_evaluate(**context):
+    model = context['ti'].xcom_pull(key='model', task_ids='train')
+    X_test = context['ti'].xcom_pull(key='X_test', task_ids='train')
+    y_test = context['ti'].xcom_pull(key='y_test', task_ids='train')
     evaluate(model, X_test, y_test)
 
-def step_5_save():
-    save()
+# 5. Сохранение
+def step_5_save(**context):
+    model = context['ti'].xcom_pull(key='model', task_ids='train')
+    save(model)
 
 with DAG(
     dag_id='ml_pipeline',
